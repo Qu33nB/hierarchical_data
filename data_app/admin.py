9245d44f05213c131bfda931a1701207904baea3
Qu33nB <bdcotton52@gmail.com>
2020-06-12 00:03:09 -0500

    finished demo

--- data_app/admin.py
+++ data_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 from data_app.models import Data
+
 # Register your models here.
 
-admin.site.register(Data, MPTTModelAdmin)
+admin.site.register(Data, DraggableMPTTAdmin)
